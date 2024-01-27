a96dae31954268d647d9b48e3958b6b3326193ba
Armando Diaz <119072544+adiazt01@users.noreply.github.com>
2024-01-27 16:43:04 -0400

    Refactor styles and add scroll event listener

---
+++ js/constants/constants.js
@@ -0,0 +1,6 @@
+export const CLASS_BG_PRIMARY = "bg-primary";
+export const CLASS_BG_WHITE = "bg-white";
+export const CLASS_BG_TRANSPARENT = "bg-transparent";
+export const CLASS_SHADOW = "shadow";
+export const CLASS_LINK_LIGHT = "link-nav-light";
+export const CLASS_LINK_PRIMARY = "link-nav-primary";
---
+++ js/utils/class.js
@@ -0,0 +1,11 @@
+export function toggleClasses(element, addClasses, removeClasses) {
+  element.classList.add(...addClasses);
+  element.classList.remove(...removeClasses);
+}
+
+export function toggleLinkClasses(links, addClass, removeClass) {
+  links.forEach((link) => {
+    link.classList.add(addClass);
+    link.classList.remove(removeClass);
+  });
+}
